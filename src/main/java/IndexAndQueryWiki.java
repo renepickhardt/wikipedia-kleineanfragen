5d8622b0051263efba3d46090d8119d5f40de88f
Rene Pickhardt <r.pickhardt@gmail.com>
2016-10-18 11:47:05 +0200

    damit hast du wohl nicht gerechnet

--- src/main/java/IndexAndQueryWiki.java
+++ src/main/java/IndexAndQueryWiki.java
@@ -4,8 +4,13 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 import org.apache.lucene.analysis.Analyzer;
@@ -31,6 +36,8 @@ import org.apache.lucene.store.FSDirectory;
 
 public class IndexAndQueryWiki {
 
+	static final int PORT = 1985;
+
 	public static void indexDocs(String pathIndex, File docFile)
 			throws IOException {
 		// Setting up the index
@@ -53,10 +60,8 @@ public class IndexAndQueryWiki {
 
 				Document d = new Document();
 
-				d.add(new StringField("title", title,
-						Field.Store.YES));
-				d.add(new TextField("text", text,
-						Field.Store.NO));
+				d.add(new StringField("title", title, Field.Store.YES));
+				d.add(new TextField("text", text, Field.Store.NO));
 
 				writer.addDocument(d);
 				if (cnt % 10000 == 0)
@@ -68,18 +73,110 @@ public class IndexAndQueryWiki {
 		writer.close();
 	}
 
+	public static void mapper(String pathIndex) throws IOException,
+			ParseException {
+		Path indexPath = Paths.get(pathIndex);
+		Directory fsdir = FSDirectory.open(indexPath);
+		Analyzer analyzer = new StandardAnalyzer();
+		IndexReader reader = DirectoryReader.open(fsdir);
+		IndexSearcher searcher = new IndexSearcher(reader);
+		searcher.setSimilarity(new BM25Similarity());
+
+		QueryParser parser = new QueryParser("text", analyzer);
+
+		// User UI
+
+		BufferedReader in = new BufferedReader(new FileReader("ka.csv"));
+		BufferedWriter out = new BufferedWriter(
+				new FileWriter(
+						"/Users/rpickhardt/ownCloud/websciencemooc/part2/LESSONS/modellingText/scripts/hrv/ka2wiki.tsv"));
+
+		String line = null;
+		int cnt = 0;
+		BooleanQuery.setMaxClauseCount(20000);
+		while ((line = in.readLine()) != null) {
+			cnt++;
+			System.out.println();
+			String[] values = line.split("\t");
+			String url = values[0];
+			String queryString = values[1];
+			try {
+				Query q = parser.parse(queryString);
+				TopDocs top10 = searcher.search(q, 10);
+				ScoreDoc[] results = top10.scoreDocs;
+				String outString = url;
+				for (int i = 0; i < results.length; i++) {
+					float score = results[i].score;
+					Document d = searcher.doc(results[i].doc);
+					outString += "\thttps://de.wikipedia.org/wiki/"
+							+ d.get("title").replace(' ', '_') + "\t" + score;
+				}
+				if (cnt % 5 == 0)
+					System.out.println(cnt + " anfragen verarbeitet.");
+				out.write(outString + "\n");
+				out.flush();
+
+			} catch (Exception e) {
+				System.out.println(url + " IMPOSSIBLE \t\t" + e.toString());
+			}
+		}
+		out.close();
+		reader.close();
+	}
+
 	public static void main(String[] args) throws IOException, ParseException {
 		String indexPath = "de-wiki-20161001-index";
-		
-		File articleData = new File(
-				"python/data/de-20161001-1-tabbed-article-per-line");
+
 		File dir = new File(indexPath);
 		if (!dir.exists()) {
+			System.out.println("need to create index");
 			dir.mkdirs();
+			File articleData = new File(
+					"python/data/de-20161001-1-tabbed-article-per-line");
 			IndexAndQueryWiki.indexDocs(indexPath, articleData);
 		}
+
+		BufferedReader in = new BufferedReader(new FileReader(
+				System.getProperty("user.dir") + "/python/data/ka2wiki.tsv"));
+		String line = "";
+		HashMap<String, ArrayList<String>> cache = new HashMap<String, ArrayList<String>>();
+		while ((line = in.readLine()) != null) {
+			String[] vals = line.split("\t");
+			ArrayList<String> al = new ArrayList<String>(Arrays.asList(vals));
+			al = new ArrayList<String>(al.subList(1, vals.length));
+			cache.put(vals[0], al);
+		}
+		System.out
+				.println(cache
+						.get("https://kleineanfragen.de/schleswig-holstein/18/237-einspeisemanagement"));
+
 		System.out.println("loaded or created index");
-		IndexAndQueryWiki.mapper(indexPath);
+		// IndexAndQueryWiki.mapper(indexPath);
 		System.out.println("mapping to done");
+
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+
+		try {
+			serverSocket = new ServerSocket(PORT);
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		}
+		while (true) {
+			try {
+				socket = serverSocket.accept();
+			} catch (IOException e) {
+				System.out.println("I/O error: " + e);
+			}
+			// new threa for a client
+			new ResponseThread(socket, cache).start();
+		}
+
+		// File indexCache = new File("");
+		// if (!indexCache.exists()){
+		// System.out.println("no mappings cached so far");
+		// }
+
 	}
 }
